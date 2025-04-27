773a476e28c6f973ac7442b5f6cb608e274b02f1
DeathOutsider <dubovoy6211@gmail.com>
2025-04-27 21:50:36 +0300

    First Commit

---
+++ src/main/java/me/dubovoy/bingoPlg/BingoPlg.java
@@ -0,0 +1,17 @@
+package me.dubovoy.bingoPlg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BingoPlg extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
